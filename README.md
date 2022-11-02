9cd8b6c8cd0aa15837b55670ffa894449d8ee1b1
unknown <snehalslande11@gmail.com>
2022-11-02 15:01:30 -0700

    all file added

---
+++ README.md
@@ -0,0 +1,2 @@
+# Medical_Insurance1
+To predict medical insurance charges  and cheack which variable affects the charges of insurance
---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+csv_file=_path="enter-path"
+PORT_NUMBER=5505 
+
+model_file_path=os.path.join("models","linear medical insurance model.pkl")
+
+json_file_path=os.path.join("models","project_data.json")
